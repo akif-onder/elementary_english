a6dacce723595fd91568a87f916c570ca782632a
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-12-02 22:35:06 +0300

    Add files via upload

---
+++ pages/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeS7wwL_CYEqXWJIsp3i02EDteA-TVRMY",
+  authDomain: "elementary-english-336a5.firebaseapp.com",
+  projectId: "elementary-english-336a5",
+  storageBucket: "elementary-english-336a5.appspot.com",
+  messagingSenderId: "944123054767",
+  appId: "1:944123054767:web:1de874a7fe7e57d638225a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
